67faa0efe2207a4f9130bbe4b437f620eb43ed77
xxxg9991 <153571560+xxxg9991@users.noreply.github.com>
2024-02-19 21:34:50 +0900

    Add files via upload

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -3,7 +3,9 @@ console.log(holidays);
 for (const i of holidays) {
   console.log(i);
 }
-while (holidays[0] <= "勤労感謝の日") {
-  console.log(holidays[0]);
-  i++;
+const i = holidays
+const list = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
+while (i < list.length) {
+  console.log(list[i]);
+  i++
 }
